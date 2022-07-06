fcd321d845ffc8b5562c37ab7e0853cc38d21dea
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-07-06 19:34:43 +0200

    Create PictureItem.vue

---
+++ src/components/PictureItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <img id="pict" :src=url alt="" @mouseover="hover = true" @mouseleave="hover = false">
+        <span v-if="hover">Bonjour</span>
+    </div>
+    
+    
+</template>
+
+
+<script>
+    export default {
+        props: ['url'],
+        data() {
+            return {
+                hover: false,
+            };
+        }
+    }
+</script>
+
+
+<style>
+    #pict{
+        border-radius: 16px;
+        width: 90%;
+        margin: 10px 10px;
+    }
+
+    
+</style>
