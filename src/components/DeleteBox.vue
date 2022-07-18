93bb37951745d376a5d6f3189fa0db85269afe7c
clement-gambier2 <78157383+clement-gambier2@users.noreply.github.com>
2022-07-18 11:55:34 +0200

    delete button and pop up without style

---
+++ src/components/DeleteBox.vue
@@ -0,0 +1,14 @@
+<template>
+    <section>
+        <p>Are you sure ?</p>
+        <span>Password</span>
+        <input type="password">
+        <button @click="$emit('close')">Cancel</button>
+        <button id="delete">Delete</button>
+    </section>
+</template>
+
+
+<script>
+
+</script>
--- src/components/PictureItem.vue
+++ src/components/PictureItem.vue
@@ -1,27 +1,34 @@
 <template>
-    <div>
+    <div id="container">
         <img id="pict" :src=url alt="" @mouseover="hover = true" @mouseleave="hover = false">
-        <span >{{name}}</span>
+        <span v-if="hover">{{name}}</span>
+        <button @click="showDelete = true">Delete</button>
+        <DeleteBox  v-if="showDelete" @close="showDelete = false"/>
+        <!--  -->
     </div>
-    
-    
 </template>
 
 
 <script>
+import DeleteBox from './DeleteBox.vue'
     export default {
-        props: ['url','name'],
-        data() {
-            return {
-                hover: false,
-            };
-        }
-    }
+    props: ["url", "name"],
+    data() {
+        return {
+            hover: false,
+            showDelete: false,
+        };
+    },
+    components: { DeleteBox }
+}
 </script>
 
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap');
+    #container{
+        position: relative;
+    }
     #pict{
         border-radius: 16px;
         width: 90%;
@@ -29,12 +36,16 @@
     }
     span{
         font-family: 'Montserrat';
-font-style: normal;
-font-weight: 700;
-font-size: 18px;
-line-height: 22px;
-
-/* color: #FFFFFF; */
+        font-style: normal;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 22px;
+        position: absolute;
+        top: 90%;
+        left:10%;
+        color: #FFF;
+        
+        
     }
 
     
